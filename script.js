9262736a8a98e0e1fb13d8e32d00165e43b3ae34
lukesrw <lukesrw+github@gmail.com>
2020-09-26 19:59:07 +0100

    Added popup event triggers

---
+++ script.js
@@ -0,0 +1,23 @@
+/*global window document send*/
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("sort").addEventListener("click", async () => {
+        console.log("here");
+
+        await send({
+            action: "sort"
+        });
+    });
+
+    document.getElementById("trade").addEventListener("click", async () => {
+        await send({
+            action: "auto",
+            arguments: [
+                document.getElementById("number").value,
+                document.getElementById("filter").value,
+                document.getElementById("half").value === "1",
+                document.getElementById("auto").value === "1"
+            ]
+        });
+    });
+});
